27fc17d141b35ee00d9b381145c41d1a266eb47b
JoshuaFitz <josh.s.fitzsimmons@gmail.com>
2024-08-12 09:21:40 +0800

    Created Skeleton springboot app

---
+++ src/test/java/com/EmployeeCRUDAPI/EmployeeCRUDAPI/EmployeeCrudapiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.EmployeeCRUDAPI.EmployeeCRUDAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+// C:\ProgrammingWorkSpace\LegalMatchExams\employeecrudapi\src\test\java\com\EmployeeCRUDAPI
